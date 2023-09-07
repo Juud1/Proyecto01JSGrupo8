b620dd44f90791dcaf6347b0fe80cb50a3af7b60
Micaela Judith Coca <micajudith637@gmail.com>
2023-09-07 11:26:16 -0300

    Agregado de HTML y js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("Porcentaje").onclick = function(){
+    var resultadoE = document.getElementById("resultados_valor");
+    NumeroA = Number(document.getElementById("NumeroA").value);
+    NumeroB = Number(document.getElementById("NumeroB").value);
+    let resultado = (NumeroA*NumeroB/100);
+    console.log("El resultado del porcentaje es " +resultado);
+    resultadoE.innerHTML= resultado;
+
+}
